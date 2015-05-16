fe26dc70a7b902d627aa00c79cebbaa73a3b91d3
ddijk <dick@dijkrosoft.nl>
2015-05-16 10:10:08 +0200

    Bean validation in Jersey

--- src/main/java/com/mycompany/beanvaljersey2/ApplicationConfig.java
+++ src/main/java/com/mycompany/beanvaljersey2/ApplicationConfig.java
@@ -1,9 +1,24 @@
 package com.mycompany.beanvaljersey2;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
 
 @ApplicationPath("res")
-public class ApplicationConfig extends Application {
+public class ApplicationConfig extends ResourceConfig {
 
+    public ApplicationConfig() {
+        
+        System.out.println("Setting BV_SEND_ERROR_IN_RESPONSE on ");
+        property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+    }
+
+    
+    
+    
+    
+    
+    
+
+    
 }
--- src/main/java/com/mycompany/beanvaljersey2/PersonService.java
+++ src/main/java/com/mycompany/beanvaljersey2/PersonService.java
@@ -1,10 +1,12 @@
 package com.mycompany.beanvaljersey2;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -15,14 +17,20 @@ public class PersonService {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response get(@QueryParam("age") @NotNull Integer age) {
+    public Response get() {
         return Response.ok(new Person("dick", 41)).build();
     }
+    
+//    @GET
+//    @Path("id")
+//    public Response getPerson(@QueryParam("age") @NotNull Integer age) {
+//         return Response.ok(new Person("jeff", 38)).build();
+//    }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response post(Person p) {
+    public Response post(@Valid Person p) {
         System.out.println("Person: " + p);
 
 //        try {
